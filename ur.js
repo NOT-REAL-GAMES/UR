08446ffcfb311ae6dc838e9b7ab5206cb6d4debf
RB <rbwldrf@gmail.com>
2023-07-05 13:59:49 +0000

    rewrite shamelessly copied from webgpu seed

--- ur.js
+++ ur.js
@@ -1,15 +1,34 @@
-async function ur(){
-	if (!navigator.gpu) {return;}
+var canvas;
+var adapter;
+var device;
 
-	
-	const canvas = document.querySelector("#ur");
-	const adapter = await navigator.gpu.requestAdapter();
-	const device = await adapter.requestDevice({
+var queue;
+
+var context;
+
+var pipeline;
+
+var colorTex;
+var colorTexView;
+
+var depthTex;
+var depthTexView;
+
+var posBuf;
+var colBuf;
+var idxBuf;
+
+var encoder;
+
+async function init(){
+	canvas = document.querySelector("#ur");
+	adapter = await navigator.gpu.requestAdapter();
+	device = await adapter.requestDevice({
 		extensions: adapter.extensions
 	});
 
-	const queue = device.queue;
-	const context = canvas.getContext("webgpu");
+	queue = device.queue;
+	context = canvas.getContext("webgpu");
 
 	const ratio = window.devicePixelRatio || 1;
 	const presentSize = [
@@ -22,95 +41,209 @@ async function ur(){
 	context.configure({ 
 		device: device, 
 		format: canvasFormat, 
-		size: presentSize});
-	
-	// Clear the canvas with a render pass
-	const encoder = device.createCommandEncoder();
-
-	const v_data = new Float32Array([
-		-0.5, -0.5, 0.0,
-		 0.5, -0.5, 0.0,
-		 0.0, 0.7, 0.0,
-	 ]);
-
-	const buffer = device.createBuffer({
-		size: v_data.byteLength,
-		usage: GPUBufferUsage.VERTEX,
+		size: presentSize,
+		usage: 
+			GPUTextureUsage.RENDER_ATTACHMENT |
+			GPUTextureUsage.COPY_SRC,
+		alphaMode: 'opaque'
 	});
-
-	const v_mod = device.createShaderModule({code:
-		`
-		@binding(0) @group(0) var<uniform> frame : u32;
-		@vertex
-		fn vtx_main(@builtin(vertex_index) vertex_index : u32) -> @builtin(position) vec4f {
-  			const pos = array(
-    			vec2( 0.0,  0.5),
-    			vec2(-0.5, -0.5),
-    			vec2( 0.5, -0.5)
-  			);
-
-  		return vec4f(pos[vertex_index], 0, 1);
-		}
-
-		@fragment
-		fn frag_main() -> @location(0) vec4f {
-  		return vec4(1, sin(f32(frame) / 128), 0, 1);
 }
 
+async function createPipeline(){	
+	posBuf = await createBuffer(positions, GPUBufferUsage.VERTEX);
+	colBuf = await createBuffer(colors, GPUBufferUsage.VERTEX);
+	idxBuf = await createBuffer(indices, GPUBufferUsage.INDEX);
+
+	const vModule = device.createShaderModule({code:
+		`
+		struct VSOut {
+			@builtin(position) Position: vec4f,
+			@location(0) color: vec3f,
+		 };
+		
+		@vertex
+		fn main(@location(0) inPos: vec3f,
+				@location(1) inColor: vec3f) -> VSOut {
+			var vsOut: VSOut;
+			vsOut.Position = vec4f(inPos, 1);
+			vsOut.color = inColor;
+			return vsOut;
+		}
+		
 		`
 	});
 
-	const layout = device.createPipelineLayout({
-		bindGroupLayouts: [],
+	const fModule = device.createShaderModule({code:
+	`
+	@fragment
+	fn main(@location(0) inColor: vec3f) -> @location(0) vec4f {
+    	return vec4f(inColor, 1);
+	}
+
+	`
 	});
 
-	const vertex = {
-		module: v_mod,
-		entryPoint:'vtx_main'
-
+	const posBufDesc = {
+		attributes: [{
+			shaderLocation: 0,
+			offset: 0,
+			format: 'float32x3'
+		}],
+		arrayStride: 4*3,
+		stepMode: 'vertex'
 	};
 
-	const rp = device.createRenderPipeline({
+	const colBufDesc = {
+		attributes: [{
+			shaderLocation: 1,
+			offset: 0,
+			format: 'float32x3'
+		}],
+		arrayStride: 4*3,
+		stepMode: 'vertex'
+	};
+
+	const depthStencil = {
+		depthWriteEnabled: true,
+		depthCompare: 'less',
+		format: 'depth24plus-stencil8'
+	};
+
+	const pipelineLayoutDesc = {bindGroupLayouts: []};
+	const layout = device.createPipelineLayout(pipelineLayoutDesc);
+
+	const vertex = {
+		module: vModule,
+		entryPoint: 'main',
+		buffers: [posBufDesc,colBufDesc]
+	};
+
+	const colorState = {format: 'bgra8unorm'};
+
+	const fragment = {
+		module: fModule,
+		entryPoint: 'main',
+		targets: [colorState]
+	};
+
+	const primitive = {
+		frontFace: 'cw',
+		cullMode: 'none',
+		topology: 'triangle-list'
+	};
+
+	const pipelineDesc = {
 		layout: layout,
-		primitive: {
-			topology: "triangle-list"
-		},
-		fragment: {
-			targets: [{
-				format: 'rgba8unorm'
-			}],
-			module: v_mod,
-			entryPoint: 'frag_main'
-		},
-		vertex: {
-			buffers: [{
-				arrayStride: 4*3,
-				attributes: [{
-					format: 'float32',
-					offset:0,
-					shaderLocation: 0
-				}]
-			}],
-			module: v_mod,
-			entryPoint: 'vtx_main'
-		}
+		vertex: vertex,
+		fragment: fragment,
+		primitive: primitive,
+		depthStencil: depthStencil
+	};
 
-		});
-  
+	pipeline = device.createRenderPipeline(pipelineDesc);
+}
 
-	const pass = encoder.beginRenderPass({
-		colorAttachments: [{
-				view: context.getCurrentTexture().createView(),
-				loadOp: "clear",
-				clearValue: { r: 0, g: 0, b: 0, a: 1.0 },
-				storeOp: "store",
-			}]
-	});
+async function createBuffer(array,usage){
+	let desc = {
+		size: (array.byteLength + 3) & ~3,
+		usage,
+		mappedAtCreation: true
+	};
+	let buffer = device.createBuffer(desc);
+	//TODO: write switch case for every possible usage.
+	const writeArray =
+		usage == GPUBufferUsage.VERTEX
+		? new Float32Array(buffer.getMappedRange())
+		: new Uint16Array(buffer.getMappedRange());
+	writeArray.set(array);
+	buffer.unmap();
+	return buffer;
+}
 
-	pass.setPipeline(rp);
-	pass.setVertexBuffer(0, buffer, [0]);
-	pass.draw(3, 1, 0, 0);
 
+//TODO: set all this info up to the ur func into separate file
+const positions = new Float32Array([
+    1.0, -1.0, 0.0, 
+    -1.0, -1.0, 0.0, 
+    0.0, 1.0, 0.0,
+    0.0, 5.0, 0.0
+]);
+
+const colors = new Float32Array([
+    1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 0.0, 1.0
+]);
+
+const indices = new Uint16Array([0, 1, 2, 0, 1, 3]);
+
+async function ur(){
+	if (!navigator.gpu) {return;}
+
+	await init();
+
+	const depthTexDesc = {
+		size: [canvas.width, canvas.height, 1],
+		dimension: '2d',
+		format: 'depth24plus-stencil8',
+		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+	};
+
+	depthTex = device.createTexture(depthTexDesc);
+	depthTexView = depthTex.createView();
+
+
+	await createPipeline();
+		
+	colTex = context.getCurrentTexture();
+	colTexView = colTex.createView();
+
+	let colorAttachment = {
+		view: colTexView,
+		clearValue: {r:0,g:0,b:0,a:1},
+		loadOp: 'clear',
+		storeOp: 'store'
+	};
+
+	const depthAttachment = {
+		view: depthTexView,
+		depthClearValue: 1,
+		depthLoadOp: 'clear',
+		depthStoreOp: 'store',
+		stencilClearValue: 0,
+		stencilLoadOp: 'clear',
+		stencilStoreOp: 'store'
+	}
+
+	const renderPassDesc = {
+		colorAttachments: [colorAttachment],
+		depthStencilAttachment: depthAttachment
+	};
+
+	encoder = device.createCommandEncoder();
+
+	const pass = encoder.beginRenderPass(renderPassDesc);
+	pass.setPipeline(pipeline);
+
+	pass.setViewport(
+		0,
+		0,
+		canvas.width,
+		canvas.height,
+		0,
+		1
+	);
+	pass.setScissorRect(
+		0,
+		0,
+		canvas.width,
+		canvas.height
+	);
+
+	pass.setVertexBuffer(0, posBuf);
+	pass.setVertexBuffer(1, colBuf);
+	pass.setIndexBuffer(idxBuf,'uint16');
+	pass.drawIndexed(indices.length,1);
 	pass.end();
 
 	device.queue.submit([encoder.finish()]);
